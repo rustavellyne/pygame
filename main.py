36bf6add621957a329c8bbfbe7cb7170a81a5c56
Alex Cherniakov <rustavellyne@gmail.com>
2026-02-09 22:38:01 +0200

    jump hero

--- main.py
+++ main.py
@@ -9,28 +9,38 @@ main_font = pygame.font.Font('./assets/font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('./assets/graphics/Sky.png').convert()
 ground_surface = pygame.image.load('./assets/graphics/ground.png').convert()
-text_surface = main_font.render('My Game', False, 'Green')
+score_surface = main_font.render('My Game', False, (64, 64, 64))
+score_rect = score_surface.get_rect(center=(400,50))
 
 snail_surface = pygame.image.load('./assets/graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom=(600, 300));
 player_surface = pygame.image.load('./assets/graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surface.get_rect(midbottom=(50,300))
+player_gravity = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            if player_rect.bottom >= 300: 
+                player_gravity = -20
     
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (350, 50))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+    screen.blit(score_surface, score_rect)
     if snail_rect.left < -100:
         snail_rect.left = 800
     if player_rect.left > 800:
         player_rect.left = -50
     snail_rect.left -= 1
     player_rect.left += 1
+    player_gravity += 1;
+    player_rect.bottom += player_gravity;
+    if player_rect.bottom >= 300: player_rect.bottom = 300
     screen.blit(snail_surface, snail_rect)
     screen.blit(player_surface, player_rect)
     # draw all elements
